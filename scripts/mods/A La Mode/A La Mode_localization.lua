b52ca0946ee0474864f9d9911fe4c08e34acd1a4
xsSplater - СанСаныч <xssplater@gmail.com>
2026-04-12 19:34:45 +0300

    Update RU A La Mode_localization.lua

--- scripts/mods/A La Mode/A La Mode_localization.lua
+++ scripts/mods/A La Mode/A La Mode_localization.lua
@@ -1,11 +1,14 @@
 return {
 	mod_description = {
 		en = "A La Mode outlines the weapon state in different colours for weapons with a toggleable state",
+		ru = "A La Mode - Меняет цвет оружия в интерфейсе в зависимости от выбранного режима",
 	},
 	mod_name = {
-		en = "A La Mode"
+		en = "A La Mode",
+		ru = "Модный",
 	},
 	alm_open_setup = {
-		en = "Open Settings Window"
+		en = "Open Settings Window",
+		ru = "Открыть окно настроек",
 	}
 }
